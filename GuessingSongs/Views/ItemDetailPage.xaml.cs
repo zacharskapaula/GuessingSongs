2da1f4d7b3aced6d58423f2e444cde94b2df32f3
zacharskapaula <84339270+zacharskapaula@users.noreply.github.com>
2022-06-26 18:32:58 +0200

    Initial commit

---
+++ GuessingSongs/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GuessingSongs.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GuessingSongs.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
